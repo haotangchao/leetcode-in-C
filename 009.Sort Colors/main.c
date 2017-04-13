fb7c621c6f6d48e7150abf5eb81020b9d1d8a29f
haotangchao <344871110@qq.com>
2017-04-13 13:01:47 +0800

    Create main.c

---
+++ 009.Sort Colors/main.c
@@ -0,0 +1,25 @@
+void swap(int* a, int* b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sortColors(int* nums, int numsSize) {
+    if (nums == NULL || numsSize < 2) {
+        return;
+    }
+    int low = 0, high = numsSize - 1;
+    int i;
+    for (i = 0; i < high;) {
+        if (nums[i] == 0) {
+            swap(&nums[i++], &nums[low++]);
+        }
+        else if (nums[i] == 2) {
+            swap(&nums[i], &nums[high--]);
+        }
+        else{
+            i++;
+        }
+    }
+}
